7a0e91fabc0711f810604d6d6b0da0c150152416
Juan rojas <davidr2998@gmail.com>
2023-04-11 18:29:28 -0500

    chore 🔧: Packages and configs
    Added packages and configs

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    dangerouslyAllowSVG: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "source.boringavatars.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
