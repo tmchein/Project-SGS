1295a646fa7cd08a7db98ecebd1c81402a2cfd46
Juan rojas <davidr2998@gmail.com>
2023-04-11 19:22:56 -0500

    fix 🐛: Fixing pointless stuff
    Fixing visitor image

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,10 +3,12 @@ import { pickRandomNames } from "@/utils/randomVisitorNames";
 import { clsx } from "clsx";
 import { Inter } from "next/font/google";
 import Image from "next/image";
+import React from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const [name] = React.useState<string>(() => pickRandomNames());
   return (
     <main>
       <header className="bg-slate-100 py-4 flex justify-between px-8">
@@ -18,7 +20,7 @@ export default function Home() {
         <div className="flex gap-4 items-center justify-center">
           <p>Welcome visitor</p>
           <Image
-            src={`https://source.boringavatars.com/beam/120/${pickRandomNames()}?colors=264653,2a9d8f,e9c46a,f4a261,e76f51`}
+            src={`https://source.boringavatars.com/beam/120/${name}?colors=264653,2a9d8f,e9c46a,f4a261,e76f51`}
             alt="User image placeholder"
             width={24}
             height={24}
